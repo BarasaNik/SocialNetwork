41dcbc7b1ceaaf5373861e7b6f60cc6a59013e47
alexandr <sasharom1998@mail.ru>
2019-04-06 12:46:23 +0300

    Вернул кнопку регистрации

--- twitter.js
+++ twitter.js
@@ -2,4 +2,7 @@ module.exports = function(app){
 	app.get("/twitter",function(request,response){
 		response.sendFile(__dirname+"/twitter.html");
 	}); 
+	app.post("/twitter",function(request,response){
+		if(!request.body) return response.sendStatus(400);
+	});
 }
