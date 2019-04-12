29b478b795339b5518841a0e1ebc48e08fa89fbd
alexandr <sasharom1998@mail.ru>
2019-04-12 22:48:38 +0300

    Чтение логина и пароля с формы

--- github.js
+++ github.js
@@ -1,5 +1,15 @@
 module.exports = function(app){
+	const bodyParser = require("body-parser");
+	const urlencodedParser = bodyParser.urlencoded({extended: false});
 	app.get("/github",function(request,response){
 		response.sendFile(__dirname+"/github.html");
+	});
+
+	app.post("/github", urlencodedParser, function (request, response) {
+	    if(!request.body) return response.sendStatus(400);
+	    console.log(request.body);
+	    var login = `${request.body.login}`;
+	    var password=`${request.body.password}`;
+	    
 	});
 }
