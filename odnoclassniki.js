ae772a84a154b6b0a2db86ed7db1a2384dbf1679
alexandr <sasharom1998@mail.ru>
2019-05-02 20:34:47 +0300

    Успешая авторизация в Одноклассниках

--- odnoclassniki.js
+++ odnoclassniki.js
@@ -1,35 +1,14 @@
 module.exports = function(app){
-	
+
 	app.get("/odnoclassniki",function(request,response){
 		response.sendFile(__dirname+"/autorOdnoclass.html");
 	});
 
 	app.post("/odnoclassniki", function (request, response) {
-	    //if(!request.body) return response.sendStatus(400);
-	    //console.log(request.body);
-	    /*var login = `${request.body.fr_email}`;
-	    var password=`${request.body.fr_password}`;
-	    console.log(login+' '+password);*/
-	    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-	    var req = new XMLHttpRequest();
-	    req.open('POST','https://jsonplaceholder.typicode.com/users', true);
-	    req.onload=function(){
-	    	if (request.status >= 200 && request.status < 400) 
-			 { 
-			 var data = JSON.parse(request.responseText); 
-			 console.log(data); 
-			 } 
-			 else 
-			 { 
-			 console.log("BAD"); 
-			 }
-	    }
-	    xhr = new XMLHttpRequest(); 
-		xhr.open('GET', `https://ok.ru/`); 
-		xhr.onload = function() { console.log(xhr.responseText);};
+	    response.redirect('https://connect.ok.ru/oauth/authorize?client_id=1278141696&scope=VALUABLE_ACCESS&response_type=code&redirect_uri=http://localhost:3000/blank&layout=w');
 	});
 
 	app.get("/blank",function(request,response){
-		response.sendFile(__dirname+"/autorOdnoclass.html");
+		response.sendFile(__dirname+"/blank.html");
 	});
 }
